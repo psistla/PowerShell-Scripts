bf0dbf64fa4b22ec3387ea7f6f2ab010b7859c2e
psistla <psistlaw@gmail.com>
2022-03-01 16:25:09 -0500

    Add files via upload

---
+++ Power Apps/GetPowerAppsInventory.ps1
@@ -0,0 +1,49 @@
+﻿
+ConnectPowerApps
+
+# get Date
+$EDate = Get-Date -Format "MMddyyyy"
+$ExportBase = Read-host "Enter Export Location (Example: C:\PowerPlatform) "
+$Folder = $ExportBase + "\" + $EDate
+
+#If folder doens't exists, folder is created.
+If (!(Test-Path $Folder)) {
+    New-Item -ItemType Directory -Force -Path $Folder
+    Write-host -f Green "Folder created in path: " + $ExportBase
+}
+
+$logpath = $Folder + "\" + "PowerAppsInventory.csv"
+
+$powerApps = Get-AdminPowerApp 
+
+$AllPowerApps = @()
+
+# loop through each app
+foreach ($powerApp in $powerApps) {
+    # loop through each connection reference for the respective APP
+    foreach ($connectionReference in $powerApp.Internal.properties.connectionReferences) {
+        #loop through each connection from the connection reference
+        foreach ($connection in $connectionReference) {
+            foreach ($connectionId in ($connection | Get-Member -MemberType NoteProperty).Name) {
+                #get the connection details
+                $connectionDetails = $($connection.$connectionId)
+
+                #prep row
+                $csvRow = @{
+                    AppDisplayName       = $powerApp.displayName
+                    AppName              = $powerApp.appName
+                    EnvironmentName      = $powerApp.environmentName
+                    ConnectorDisplayName = $connectionDetails.displayName
+                    ConnectionId         = $connectionDetails.id
+                    ConnectionName       = $connectionDetails.connectionName
+                    CreatedByEmail       = $powerApp.owner.email
+                    IsPremiumConnector   = $connectionDetails.apiTier -eq 'Premium'
+                }
+                $AllPowerApps += $(new-object psobject -Property $csvRow)
+            }
+        }        
+    }
+}
+
+# output to file
+$AllPowerApps | Export-Csv -Path $logpath
---
+++ Power Apps/PowerAppsAndFlows.ps1
@@ -0,0 +1,59 @@
+﻿# Import-Module Microsoft.PowerApps.Administration.PowerShell
+
+# get Date
+$EDate = Get-Date -Format "MMddyyyy"
+$ExportBase = Read-host "Enter Export Location (Example: C:\PowerPlatform) "
+$Folder = $ExportBase + "\" + $EDate
+
+#If folder doens't exists, folder is created.
+If (!(Test-Path $Folder)) {
+    New-Item -ItemType Directory -Force -Path $Folder
+    Write-host -f Green "Folder created in path: " + $ExportBase
+}
+
+$logpath = $Folder + "\" + "AppsAndFlows.csv"
+   
+If (Test-Path $logpath) {
+    Remove-Item $logpath
+}
+"Type" + "," + "DisplayName" + "," + "OwnerEmail" | Out-File $logpath -Encoding ascii
+    
+Add-PowerAppsAccount
+    
+$flows = Get-AdminFlow
+$powerApps = Get-AdminPowerApp
+
+
+#Flows
+foreach ($flow in $flows) {
+    $flowDetails = $flow | Get-AdminFlow
+    $flowName = $flow.DisplayName -replace '[,]'
+    $flowOwnerObj = Get-UsersOrGroupsFromGraph -ObjectId $flow.createdBy.objectId
+    $ownerUPN = $flowOwnerObj.UserPrincipalName
+    foreach ($connector in $flowDetails.Internal.properties.connectionReferences) {
+        foreach ($connection in ($connector | gm -MemberType NoteProperty).Name) {
+                
+            "Flow" + "," + $flowName + "," + $ownerUPN | Out-File $logpath -Encoding ascii -Append
+               
+        }
+    }
+}
+
+
+#Apps
+foreach ($app in $powerApps) {
+    $appName = $app.DisplayName
+    $ownerObject = $app.owner.id
+    $appOwnerObj = Get-UsersOrGroupsFromGraph -ObjectId $ownerObject
+    $ownerUPN = $appOwnerObj.UserPrincipalName
+    foreach ($connector in $app.Internal.properties.connectionReferences) {
+        foreach ($connection in ($connector | gm -MemberType NoteProperty).Name) {
+            $connectionProperties = $($connector.$connection)
+            write-host $connectionProperties.DisplayName
+                
+            "PowerApp" + "," + $appName + "," + $ownerUPN | Out-File $logpath -Encoding ascii -Append
+                
+        }
+    }
+}
+#endregion
